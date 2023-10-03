1b0d04479ab493b50f1998a17b561c1426a79995
dev-dino22 <wlsskdud02@naver.com>
2023-10-03 17:04:33 +0900

    [ Initial Setting ] Record,Sound,Edit install library

--- android/app/src/components/CameraUI/RecFnc.tsx
+++ android/app/src/components/CameraUI/RecFnc.tsx
@@ -54,12 +54,10 @@ const RecFnc: React.FC<RecFncProps> = ({ cameraRef }) => {
     }
 
     toggleRecording();
-    // 첫 렌더링이 아니라는 것을 표시
     isFirstRender.current = false;
 
     return () => {
       iSound?.stop();
-      //클린업함수실행
     };
   }, [isRecording]);
 
