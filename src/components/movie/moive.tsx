ae3edd2457f8869ce6b593f3444c2e349dc08bd9
bragging <kps9616@gmail.com>
2023-11-07 22:06:36 +0900

    메인 페이지 html 추가 및 폴더 분류 추가

---
+++ src/components/movie/moive.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View } from 'react-native';
+import CameraUI from './android/app/src/components/CameraUI/CameraUI';
+import { CameraUIProvider } from './android/app/src/contexts/CameraUIContext';
+import EditUI from './android/app/src/components/EditUI/EditUI';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { enableScreens } from 'react-native-screens'; // import enableScreens
+import SelectMusicUI from './android/app/src/components/SelectMusicUI/SelectMusicUI';
+
+enableScreens(); // Call enableScreens to activate react-native-screens
+
+
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  // @ts-ignore
+  return (
+    <>
+      <NavigationContainer>
+        <View style={{ flex: 1, backgroundColor: 'black' }}>
+          <CameraUIProvider>
+            <Stack.Navigator initialRouteName="CameraUI"
+              screenOptions={{
+                headerShown: false
+              }}>
+              <Stack.Screen name="CameraUI" component={CameraUI} />
+              <Stack.Screen name="EditUI" component={EditUI} />
+              <Stack.Screen name="SelectMusicUI" component={SelectMusicUI} />
+            </Stack.Navigator>
+          </CameraUIProvider>
+        </View>
+      </NavigationContainer>
+    </>
+  );
+}
+
+export default App;
