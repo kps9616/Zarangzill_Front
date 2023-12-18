f7376ee5f363b2307f80f2e6ce39339c999fdf44
bragging <kps9616@gmail.com>
2023-12-18 22:53:09 +0900

    하단 네비게이터 및 페이지 연결

---
+++ src/components/header/header-sheets.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "react-native-gesture-handler";
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import SearchComponent from "../search/search-component";
+import {createNativeStackNavigator} from "react-native-screens/native-stack";
+
+const Tab = createNativeStackNavigator();
+
+function HeaderTabNavigationApp() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="SearchSf"
+                component={SearchComponent}
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
+export default HeaderTabNavigationApp;
--- src/components/shotform/shotformView.tsx
+++ src/components/shotform/shotformView.tsx
@@ -1,12 +1,52 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {StyleSheet, View, Text, Alert, Image, TouchableOpacity} from "react-native";
+import Video from "react-native-video";
+import {useNavigation} from "@react-navigation/native";
 
 function shotformView() {
+    const navigation = useNavigation();
+    const earthVideo = require('../../../assets/svideo.mp4'); //로컬 파일 사용 방법
+    const searchIcon = require('../../../images/icon/crown-yellow.png'); //로컬 파일 사용 방법
     return (
-        <View>
-            <Text>Start Shot form!</Text>
+        <View style={styles.container}>
+            {/*<Video
+                source={earthVideo }
+                style={styles.fullScreen}
+                paused={false} // 재생/중지 여부
+                resizeMode={"cover"} // 프레임이 비디오 크기와 일치하지 않을 때 비디오 크기를 조정하는 방법을 결정합니다. cover : 비디오의 크기를 유지하면서 최대한 맞게
+                onLoad={e => console.log(e)} // 미디어가 로드되고 재생할 준비가 되면 호출되는 콜백 함수입니다.
+                repeat={false} // video가 끝나면 다시 재생할 지 여부
+
+            />*/}
+            <TouchableOpacity onPress={() => navigation.navigate("SearchSf")}>
+                <Image source={searchIcon} style={styles.search} />
+            </TouchableOpacity>
         </View>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white"
+    },
+    fullScreen: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+    },
+    search: {
+        position:"absolute",
+        top:16,
+        left:100,
+        width:24,
+        height:24,
+        zIndex:99
+    }
+});
+
 
 export default shotformView;
