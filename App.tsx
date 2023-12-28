290f633e51af83e76352e7af6cb2e42cf088fd84
bragging <kps9616@gmail.com>
2023-12-28 22:05:00 +0900

    App.tsx 수정, apk 릴리즈 시 http 실행 되도록 옵션 추가

--- App.tsx
+++ App.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
 import BottomSheets from "./src/components/bottom-sheets/bottom-sheets";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "react-native-screens/native-stack";
-import SearchComponent from "./src/components/search/search-component";
-import ShotformView from "./src/components/shotform/shotformView";
-import WinnerView from "./src/components/winner/winnerView";
-import FansView from "./src/components/fans/fansView";
-import MypagesView from "./src/components/mypage/mypagesView";
-import { CameraUIProvider } from './android/app/src/contexts/CameraUIContext';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "react-native-screens/native-stack";
+import Svg, {Path} from "react-native-svg";
+import AppContext from './AppContext';
+import {CameraUIProvider} from "./android/app/src/contexts/CameraUIContext";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
 
-  return (
+    const [stringValue, setStringValue] = useState('http://1.226.83.35:8090');
 
-    <NavigationContainer>
-      <CameraUIProvider>
+    const setValue = (string: React.SetStateAction<string>) => {
+        setStringValue(string)
+    }
+
+
+    const values = {
+        stringValue: stringValue,
+        setValue
+    }
+
+  return (
+      <AppContext.Provider value={values}>
+      <NavigationContainer>
+
+          <CameraUIProvider>
         <Stack.Navigator initialRouteName="BottomSheets">
           <Stack.Screen
-            name="SearchSf"
-            component={SearchComponent}
-            options={{ headerShown: false }}
-          >
-          </Stack.Screen>
-          <Stack.Screen
-            name="BottomSheets"
-            component={BottomSheets}
-            // stack navigator를 중첩할때마다 헤더가 하나씩 늘어나기 때문에 header hide처리
-            // (추후 bottomTab 하위에 shared stack이 추가되는 케이스 고려)
-            options={{ headerShown: false }}
-          >
-          </Stack.Screen>
+              name="BottomSheets"
+              component={BottomSheets}
+              options={{headerShown: false}}
+          />
         </Stack.Navigator>
-      </CameraUIProvider>
-    </NavigationContainer>
+          </CameraUIProvider>
+      </NavigationContainer>
+      </AppContext.Provider>
   );
 
 
